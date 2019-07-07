552b1b5fc4909b61037601cd12d1aa28561dea22
Administrator <zjtyxy@163.com>
2019-07-07 15:30:20 +0800

    new

--- src/main/java/cn/lger/controller/MiniAppController.java
+++ src/main/java/cn/lger/controller/MiniAppController.java
@@ -5,14 +5,22 @@ import cn.lger.domain.Commodity;
 import cn.lger.domain.Gift;
 import cn.lger.domain.Member;
 import cn.lger.domain.MemberGrade;
+
+import org.apache.coyote.Response;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.util.List;
 
 @RestController
@@ -76,6 +84,36 @@ public class MiniAppController {
         }
         Pageable pageable = new PageRequest(currentPage, 3);
         return commodityDao.findAll(pageable);
+    }
+    @RequestMapping(value = "/minapp/upload", method = RequestMethod.POST)
+    public String uploadImage(@RequestParam(value = "file") MultipartFile file) throws RuntimeException {
+        if (file.isEmpty()) {
+            return "文件不能为空";
+        }
+        // 获取文件名
+        String fileName = file.getOriginalFilename();
+       // logger.info("上传的文件名为：" + fileName);
+        // 获取文件的后缀名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+       // logger.info("上传的后缀名为：" + suffixName);
+        // 文件上传后的路径
+        String filePath = "d:/upload/images/";
+        // 解决中文问题，liunx下中文路径，图片显示问题
+        // fileName = UUID.randomUUID() + suffixName;
+        File dest = new File(filePath + fileName);
+        // 检测是否存在目录
+        File parent = dest.getParentFile();
+        if (!parent.exists()) {
+           parent.mkdirs();
+        }
+        try {
+            file.transferTo(dest);
+          //  logger.info("上传成功后的文件路径未：" + filePath + fileName);
+            return  fileName;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "文件上传失败";
     }
 
 
