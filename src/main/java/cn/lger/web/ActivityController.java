752218b972dcefc570b10e8d5b74dd7ca20bce0b
Administrator <zjtyxy@163.com>
2019-07-22 12:11:39 +0800

    活动页面

--- src/main/java/cn/lger/web/ActivityController.java
+++ src/main/java/cn/lger/web/ActivityController.java
@@ -11,10 +11,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -36,18 +33,47 @@ public class ActivityController {
     }
 
     @GetMapping("/queryActivity")
-    public String getQueryMemberView(Model model, @RequestParam(defaultValue = "0",required = false) Integer pageNum) {
-        if(pageNum == null)
-            pageNum=0;
+    public String getQueryMemberView(Model model, @RequestParam(defaultValue = "0", required = false) Integer pageNum) {
+        if (pageNum == null)
+            pageNum = 0;
         //  Sort sort = Sort.by(new Sort.Order(Sort.Direction.DESC,"level"));
-        Pageable page =PageRequest.of(pageNum,10);
-        Page<Activity> pages=activityDao.findAll(page);
-        model.addAttribute("entitylist",pages.getContent());
-        model.addAttribute("page",pages);
-        model.addAttribute("pageNum",pageNum);
-        model.addAttribute("totalPages",pages.getTotalPages());
-        model.addAttribute("totalElements",pages.getTotalElements());
+        Pageable page = PageRequest.of(pageNum, 10);
+        Page<Activity> pages = activityDao.findAll(page);
+        model.addAttribute("entitylist", pages.getContent());
+        model.addAttribute("page", pages);
+        model.addAttribute("pageNum", pageNum);
+        model.addAttribute("totalPages", pages.getTotalPages());
+        model.addAttribute("totalElements", pages.getTotalElements());
         return "queryActivity";
     }
 
+    /**
+     * 修改新建都调用这个方法，修改时要带参数id
+     * @param model
+     * @param activity
+     * @return
+     */
+    @RequestMapping("/addActivity")
+    public String addActivity(Model model, Activity activity){
+        Activity rst = activity;
+        if(activity.getId()!=null)
+        {
+            rst =  activityDao.findById(activity.getId()).get();
+        }
+        model.addAttribute("entity",rst);
+        return "modifyActivity";
+    }
+
+    /**
+     * 修改或者添加提交调用
+     * @param model
+     * @param activity
+     * @return
+     */
+    @RequestMapping("/modifyActivity")
+    public String modifyActivity(Model model,Activity activity) {
+        activityDao.save(activity);
+        model.addAttribute("entity",activity);
+        return "redirect:queryActivity";
+    }
 }
