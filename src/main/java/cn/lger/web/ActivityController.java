56104e786732225dec77a65ebb7601a6c88ecf91
Administrator <zjtyxy@163.com>
2019-08-02 23:40:24 +0800

    换layui

--- src/main/java/cn/lger/web/ActivityController.java
+++ src/main/java/cn/lger/web/ActivityController.java
@@ -9,8 +9,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Optional;
 
 @Controller
@@ -107,4 +113,87 @@ public class ActivityController {
         }
       return "fail";
     }
+    @RequestMapping(value = "/addPhoto",method = RequestMethod.POST)
+    public @ResponseBody String uploadImg(@RequestParam("file") MultipartFile file, HttpSession session,
+                                          HttpServletRequest request) {
+
+
+
+        String str="";
+
+        String league_id=request.getSession().getAttribute("leagueID").toString();
+
+        System.out.println("联赛id："+league_id);
+
+        String url=request.getRequestURI();
+        System.out.println("URL:"+url);
+
+        try {
+            if(null!=file){
+                //获得当前项目所在路径
+                String pathRoot=request.getSession().getServletContext().getRealPath("");
+                System.out.println("当前项目所在路径："+pathRoot);
+                //生成uuid作为文件名称
+             //  String uuid=UUID.randomUUID().toString().replaceAll("-","");
+             //   System.out.println("文件名称："+uuid);
+                //获得文件类型（判断如果不是图片文件类型，则禁止上传）
+                String contentType=file.getContentType();
+                System.out.println("文件类型："+contentType);
+                //获得文件后缀名称
+                String imageName=contentType.substring(contentType.indexOf("/")+1);
+                System.out.println("文件后缀名称："+imageName);
+
+                String filePath="/Users/hurley/Desktop/footballleague-managementsystem/src/main/resources/static/upload/images/";
+                //根据日期来创建对应的文件夹
+                String datePath=new SimpleDateFormat("yyyy/MM/dd/").format(new Date());
+                System.out.println("日期："+datePath);
+                //根据id分类来创建对应的文件夹
+                String leagueIdPath=league_id+"/";
+
+                //日期
+                /*String path=filePath+datePath;*/
+                //联赛id
+                String path=filePath+leagueIdPath;
+
+                //如果不存在，则创建新文件夹
+                File f=new File(path);
+                if(!f.exists()){
+                    f.mkdirs();
+                }
+                //新生成的文件名称
+            //    String fileName=uuid+"."+imageName;
+                String fileName="uuid"+"."+imageName;
+                System.out.println("新生成的文件名称："+fileName);
+                session.setAttribute("fileName",fileName);
+
+                //图片保存的完整路径
+                String pathName=path+fileName;
+                System.out.println(pathName);
+
+                //获取所属联赛ID
+                int leagueID=Integer.parseInt(league_id);
+
+                //图片的静态资源路径
+                String staticPath="/upload/images/"+leagueID+"/"+fileName;
+                System.out.println("静态资源路径："+staticPath);
+
+                //复制操作
+                //将图片从源位置复制到目标位置
+                file.transferTo(new File(pathName));
+
+                str = "{\"code\": 0,\"msg\": \"\",\"data\": {\"src\":\"" + "pic/"+datePath+fileName + "\"}}";
+
+
+            }
+            else{
+                System.out.println("文件为空");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            return str;
+        }
+
+    }
+
 }
